f705477b77fbbf34e1a067bb232303045e1f0317
yunuszenduserbot <79845960+yunuszenduserbot@users.noreply.github.com>
2021-04-27 10:30:56 +0000

    Play.py

--- handlers/play.py
+++ handlers/play.py
@@ -61,7 +61,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="https://telegra.ph/file/a4fa687ed647cfef52402.jpg",
         reply_markup=keyboard,
-        caption="🎧 **Memainkan** musik tersebut di VCG.
+        caption=" 🎧 **Memainkan** musik tersebut di VCG. 
 👤 Request by {}!".format(
         message.from_user.mention()
         ),
